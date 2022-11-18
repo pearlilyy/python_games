a76a3bdb93fab2c4130907e9c0331cd7c28724e4
Pearl <flairpearllee@gmail.com>
2022-11-17 20:40:13 -0800

    chatbot add

--- chatbot/main.py
+++ chatbot/main.py
@@ -13,6 +13,7 @@ pip install tensorflow
 
 import json
 import random
+from importlib_metadata import List
 import tensorflow
 import tflearn
 import numpy
@@ -26,13 +27,49 @@ with open("intents.json") as file:
 
 words = []
 labels = []
-docs = []
+docs_x = []
+docs_y = []
 
 for intent in data["intents"]:
     for pattern in intent["patterns"]:
         wrds = nltk.word_tokenize(pattern)
         words.extend(wrds)
-        docs.append(pattern)
+        docs_x.append(pattern)
+        docs_y.append(intent["tag"])
 
         if intent["tag"] not in labels:
             labels.append(intent["tag"])
+
+words = [stemmer.stem(w.lower()) for w in words]
+words = sorted(list(set(words)))
+
+labels = sorted(labels)
+
+training = []
+output = []
+
+out_empth = [0 for _ in range(len(classes))]
+
+for x, doc in enumerate(docs_x):
+    bag = []
+
+    wrds = [stemmer.stem(w) for w in doc]
+
+    for w in words:
+        if w in wrds:
+            bag.append(1)
+        else:
+            bag.append(0)
+
+    output_row = out_empty[:]
+    output_row[labels.index(docs_y[x])] = 1
+
+    training.append(bag)
+    output.append(output_row)
+
+training = numpy.array(training)
+output = numpy.array(output)
+
+tensorflow.reset_default_graph()
+
+net = tflearn.input_data(shape=[None, len(training[0])])
